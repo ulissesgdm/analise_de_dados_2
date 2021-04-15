4bb8abeac5d8a87ba5bb2c8de58fc9b7e5f47961
ulissesgdm <ulissesgdm@live.com>
2021-04-15 11:11:59 -0300

    estrutura condicional

--- Estrutura condicional.R
+++ Estrutura condicional.R
@@ -8,9 +8,24 @@ download.file(url, "banco.sav", mode = "wb")
 
 banco <- read_sav("banco.sav")
 
-
-#Histograma feito com base no interesse por polÃ­tica
-
-lapply(banco$ANOANIVER, hist)
-
 banco$D1A_ID <- ifelse(banco$D1A_ID > 60, "idoso", "não idoso") #Modifica a variável idade do ESEB 2018, separando idosos (>60) de não idosos.
+
+library(poliscidata)
+
+banco <- gss %>% 
+  filter(!is.na(sibs),
+         !is.na(childs),
+         !is.na(age))
+
+par(mfrow = c(2, 2))
+
+#Análise realizada a partir do banco gss, observando a idade o número de filhos e irmãos
+
+for (i in 7:9) {
+  x <- banco[ , i] 
+  hist(x,
+       main = names(banco)[i],
+       xlab = "Valores da Variável", 
+       ylab = 'Frequência', 
+       xlim = c(min(banco[, i]), max(banco[, i]))) 
+}
