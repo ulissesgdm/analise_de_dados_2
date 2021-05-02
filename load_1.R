e6ca1c2f710a8647d219226acc2349d10ecfb33f
ulissesgdm <ulissesgdm@live.com>
2021-05-02 17:14:04 -0300

    exportação de dados

---
+++ load_1.R
@@ -0,0 +1,70 @@
+# carrega a base de snistros de transito do site da PCR
+
+sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
+
+#modifica o nome da coluna
+
+library(dplyr)
+
+sinistrosRecife2019Raw38 <- rename(sinistrosRecife2019Raw, data = DATA)
+
+# junta as bases de dados com comando rbind (juntas por linhas)
+sinistrosRecifeRaw <- rbind(sinistrosRecife2019Raw38[,-c(10:12)], sinistrosRecife2020Raw, sinistrosRecife2021Raw)
+
+# observa a estrutura dos dados
+str(sinistrosRecifeRaw)
+
+# modifica natureza do sinistro de texto para fator
+sinistrosRecifeRaw$natureza_acidente <- as.factor(sinistrosRecifeRaw$situacao)
+
+# cria funçaõ para substituir not available (na) por 0
+naZero <- function(x) {
+  x <- ifelse(is.na(x), 0, x)
+}
+
+# aplica a função naZero a todas as colunas de contagem
+sinistrosRecifeRaw[, 15:25] <- sapply(sinistrosRecifeRaw[, 15:25], naZero)
+
+ls()
+
+for (itm in ls()) { 
+  print(formatC(c(itm, object.size(get(itm))), 
+                format="d", 
+                width=30), 
+        quote=F)
+}
+
+ls()
+
+gc()
+
+#remover bases não utilizadas
+rm(list = c('sinistrosRecife2019Raw', 'sinistrosRecife2019Raw38', 'sinistrosRecife2020Raw', 'sinistrosRecife2021Raw'))
+
+install.packages('microbenchmark')
+library(microbenchmark)
+
+saveRDS(sinistrosRecifeRaw, "sinistrosRecife.rds")
+
+write.csv2(sinistrosRecifeRaw, "sinistrosRecife.csv")
+
+library(writexl)
+write_xlsx(sinistrosRecifeRaw, "sinistrosRecife.rds")
+
+#teste salvamento
+microbenchmark(a <- saveRDS(sinistrosRecifeRaw, "sinistrosRecife.rds"), 
+               b <- write.csv2(sinistrosRecifeRaw, "sinistrosRecife.csv"),
+               c <- write_xlsx(sinistrosRecifeRaw, "sinistrosRecife.rds"), times = 30L)
+
+#teste leitura
+microbenchmark(a <- readRDS('sinistrosRecife.rds'), 
+               b <- read.csv2('sinistrosRecife.csv', sep = ';'), 
+               c <- read_excel("sinistrosRecife.rds"), times = 10L)
+
+
+
+
